6731b24e874a892866334e12ac1d50abfa1f6ec8
Temirlankz <altaytemirlan@mail.ru>
2025-05-22 20:38:03 +0300

    DAtabase and file classes was added also added a publiv

---
+++ classes/Database.php
@@ -0,0 +1,18 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "password_manager";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        } catch (Exception $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
