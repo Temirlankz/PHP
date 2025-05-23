91403efc139027f3ddcd4b53558070c520e486f4
Temirlankz <altaytemirlan@mail.ru>
2025-05-23 16:51:48 +0300

    fixed

--- classes/User.php
+++ classes/User.php
@@ -8,7 +8,8 @@ class User {
     public function register($username, $password) {
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
         $aes_key = bin2hex(random_bytes(16));
-        $aes_key_encrypted = openssl_encrypt($aes_key, 'AES-256-CBC', $password, 0, substr($password, 0, 16));
+        $iv = str_pad(substr($password, 0, 16), 16, "\0");
+        $aes_key_encrypted = openssl_encrypt($aes_key, 'AES-256-CBC', $password, 0, $iv);
 
         $stmt = $this->conn->prepare("INSERT INTO users (username, password_hash, aes_key_encrypted) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $username, $password_hash, $aes_key_encrypted);
